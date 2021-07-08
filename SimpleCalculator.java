a1ff73e63c01d7860bcce16d9091a504e2f5c7d2
Ranjana Perera <76731513+aardperera@users.noreply.github.com>
2021-07-08 18:27:22 +0530

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,12 @@
+
+package simplecalculator;
+
+
+public class SimpleCalculator {
+
+    
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.setVisible(true);
+    }
+}
